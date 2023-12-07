eb25a5e190622ec17abada8abf65ae99ff9e672b
すふぃあ <sphere.soffie@gmail.com>
2023-12-08 08:36:15 +0900

    test

---
+++ CopyTo01.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VisualStudioTest01 {
+	internal class CopyTo01 {
+		Task? task;
+		public void testMethod01() {
+			// paste here (copy from data.CopyFrom01.testMethod01).
+		}
+		public void testMethod02() {
+			// paste here (copy from data.CopyFrom01.testMethod02).
+		}
+		public void testMethod03() {
+			// paste here (copy from data.CopyFrom01.testMethod03).
+		}
+		public void testMethod04() {
+			// paste here (copy from data.CopyFrom01.testMethod04).
+		}
+
+	}
+}
---
+++ data/CopyFrom01.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualStudioTest01.data {
+	internal class CopyFrom01 {
+		public void testMethod01() {
+			// copy this to CopyTo01.testMethod01.
+			data.Task t = new Task();
+		}
+		public void testMethod02() {
+			// copy this to CopyTo01.testMethod02.
+			data.Task t = new data.Task();
+			Sample s = new Sample();
+		}
+		public void testMethod03() {
+			// copy this to CopyTo01.testMethod03.
+			data.Task t = new data.Task();
+			data.Sample s = new Sample();
+		}
+		public void testMethod04() {
+			// copy this to CopyTo01.testMethod04.
+			System.Threading.Tasks.Task.Run(() => {
+				Task t = new Task();
+			});
+		}
+	}
+}
