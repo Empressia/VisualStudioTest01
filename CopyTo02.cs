6c1d9a6a86d1cc647d8f14c9d9da5d873b106ba5
すふぃあ <sphere.soffie@gmail.com>
2023-12-09 10:04:18 +0900

    サンプルを追加しました。

---
+++ CopyTo02.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using data = VisualStudioTest01.data;
+
+namespace VisualStudioTest01 {
+	internal class CopyTo02 {
+		Task? task1;
+		data.Task? task2;
+		public void testMethod01() {
+			// paste here (copy from data.CopyFrom01.testMethod01).
+		}
+		public void testMethod02() {
+			// paste here (copy from data.CopyFrom01.testMethod02).
+		}
+		public void testMethod03() {
+			// paste here (copy from data.CopyFrom01.testMethod03).
+		}
+		public void testMethod04() {
+			// paste here (copy from data.CopyFrom01.testMethod04).
+		}
+
+	}
+}
