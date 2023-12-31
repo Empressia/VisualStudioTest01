6f64316666f3be84bf6669109e75228ef0e97898
すふぃあ <sphere.soffie@gmail.com>
2023-12-31 15:22:53 +0900

    クラス内でのもう少し現実的なサンプルを追加しました。

---
+++ SomethingLikeDBWrapper.cs
@@ -0,0 +1,27 @@
+﻿using data = VisualStudioTest01.data;
+
+namespace VisualStudioTest01 {
+	internal class SomethingLikeDBWrapper {
+		public Task register(data.Sample? sample) {
+			// copy this if statement to another method in this class.
+			if(sample == null) { throw new Exception(); }
+			return Task.Run(() => { });
+		}
+		public Task register(data.Task? task) {
+			return Task.Run(() => { });
+		}
+		public Task register(string? s) {
+			return Task.Run(() => { });
+		}
+		public Task register(int? i) {
+			return Task.Run(() => { });
+		}
+		public Task register(long? d) {
+			return Task.Run(() => { });
+		}
+		public Task register(DateTimeOffset? datetime) {
+			return Task.Run(() => { });
+		}
+
+	}
+}
